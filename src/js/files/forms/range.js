901b716dc49f92740a732828b928fd25051382db
Dmitriifd <idimka9@gmail.com>
2022-09-15 15:12:51 +0500

    range slider

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -1,42 +1,56 @@
 // Подключение из node_modules
 import * as noUiSlider from 'nouislider';
+import '../../libs/wNumb.min.js'
 
 // Подключение стилей из scss/base/forms/range.scss 
 // в файле scss/forms/forms.scss
 
 // Подключение cтилей из node_modules
-// import 'nouislider/dist/nouislider.css';
+import 'nouislider/dist/nouislider.css';
+import '../../../scss/base/forms/range-slider.scss';
+
 
 export function rangeInit() {
-	const priceSlider = document.querySelector('#range');
-	if (priceSlider) {
-		let textFrom = priceSlider.getAttribute('data-from');
-		let textTo = priceSlider.getAttribute('data-to');
-		noUiSlider.create(priceSlider, {
-			start: 0, // [0,200000]
-			connect: [true, false],
+	const rangeSlider = document.getElementById('range-slider')
+	const priceFrom = document.querySelector('.catalog-form__price-from')
+	const priceTo = document.querySelector('.catalog-form__price-to')
+
+	if (rangeSlider) {
+		noUiSlider.create(rangeSlider, {
+			start: [500, 50000],
+			connect: true,
+			// tooltips: [wNumb({ decimals: 0 }), wNumb({ decimals: 0 })],
+			step: 1,
 			range: {
-				'min': [0],
-				'max': [200000]
-			}
-		});
-		/*
-		const priceStart = document.getElementById('price-start');
-		const priceEnd = document.getElementById('price-end');
-		priceStart.addEventListener('change', setPriceValues);
-		priceEnd.addEventListener('change', setPriceValues);
-		*/
-		function setPriceValues() {
-			let priceStartValue;
-			let priceEndValue;
-			if (priceStart.value != '') {
-				priceStartValue = priceStart.value;
-			}
-			if (priceEnd.value != '') {
-				priceEndValue = priceEnd.value;
-			}
-			priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
+				min: [500],
+				max: [50000],
+			},
+		})
+
+		const input0 = document.getElementById('input-0')
+		const input1 = document.getElementById('input-1')
+		const inputs = [input0, input1]
+    const price = [priceFrom, priceTo]
+
+		rangeSlider.noUiSlider.on('update', function (values, handle) {
+      price[handle].textContent = `${Math.round(values[handle])} ₽`
+			inputs[handle].value = Math.round(values[handle])
+		})
+
+		const setRangeSlider = (i, value) => {
+			let arr = [null, null]
+			arr[i] = value
+
+			console.log(arr)
+
+			rangeSlider.noUiSlider.set(arr)
 		}
+
+		inputs.forEach((el, index) => {
+			el.addEventListener('change', (e) => {
+				setRangeSlider(index, e.currentTarget.value)
+			})
+		})
 	}
 }
 rangeInit();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,104 +1,104 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile, _slideUp } from './functions.js';
+import { isMobile, _slideUp } from './functions.js'
 // Подключение списка активных модулей
-import { flsModules } from './modules.js';
+import { flsModules } from './modules.js'
 
-const popup = document.querySelector('.header-popup');
-const popupBtnClose = document.querySelector('.header-popup__button');
-const cardCatalog = document.querySelectorAll('.card-catalog');
-const headerLabel = document.querySelector('.header-label');
+const popup = document.querySelector('.header-popup')
+const popupBtnClose = document.querySelector('.header-popup__button')
+const cardCatalog = document.querySelectorAll('.card-catalog')
+const headerLabel = document.querySelector('.header-label')
 
 /* Смена имени radio input в карточке */
 
 document.querySelectorAll('.card-catalog').forEach((card, i) => {
-	card.querySelectorAll('.size-chooser__item').forEach((input) => (input.name = `radio-${i + 1}`));
-});
+	card.querySelectorAll('.size-chooser__item').forEach((input) => (input.name = `radio-${i + 1}`))
+})
 
 /* Кнопка свернуть */
 
 try {
-	const flowersDeliveryButton = document.querySelector('.flowers-delivery__button');
-	const flowersDeliveryWrapper = document.querySelector('.flowers-delivery__wrapper');
-	const buttonText = flowersDeliveryButton.querySelector('span');
+	const flowersDeliveryButton = document.querySelector('.flowers-delivery__button')
+	const flowersDeliveryWrapper = document.querySelector('.flowers-delivery__wrapper')
+	const buttonText = flowersDeliveryButton.querySelector('span')
 
 	flowersDeliveryButton.addEventListener('click', () => {
-		flowersDeliveryWrapper.classList.toggle('active');
-		flowersDeliveryButton.classList.toggle('active');
+		flowersDeliveryWrapper.classList.toggle('active')
+		flowersDeliveryButton.classList.toggle('active')
 
 		if (flowersDeliveryButton.classList.contains('active')) {
-			buttonText.textContent = 'Развернуть текст';
+			buttonText.textContent = 'Развернуть текст'
 		} else {
-			buttonText.textContent = 'Свернуть текст';
+			buttonText.textContent = 'Свернуть текст'
 		}
-	});
+	})
 } catch (error) {
-	console.log(error);
+	console.log(error)
 }
 
 /* Favorite button */
 
-const likeBtn = document.querySelectorAll('.card__favorite-button');
+const likeBtn = document.querySelectorAll('.card__favorite-button')
 
-let counter = 0;
+let counter = 0
 likeBtn.forEach((fovoriteBtn) => {
 	fovoriteBtn.addEventListener('click', () => {
 		if (!fovoriteBtn.classList.contains('red')) {
-			counter++;
-			headerLabel.textContent = counter;
-			headerLabel.style.display = 'block';
+			counter++
+			headerLabel.textContent = counter
+			headerLabel.style.display = 'block'
 		} else {
-			counter--;
-			headerLabel.textContent = counter;
+			counter--
+			headerLabel.textContent = counter
 			if (counter === 0) {
-				headerLabel.style.display = 'none';
+				headerLabel.style.display = 'none'
 			}
 		}
-	});
-});
+	})
+})
 
 likeBtn.forEach((btn) => {
 	btn.addEventListener('click', ({ target }) => {
 		if (target.classList.contains('card__favorite-button')) {
-			btn.classList.toggle('red');
+			btn.classList.toggle('red')
 		}
 		if (target.classList.contains('card__favorite-button--catalog')) {
-			btn.classList.toggle('green');
+			btn.classList.toggle('green')
 		}
-	});
-});
+	})
+})
 
 /* NavBar */
 
-const navbarlink = document.querySelectorAll('.navbar-mobile__link');
-const navbarList = document.querySelector('.navbar-mobile__list');
+const navbarlink = document.querySelectorAll('.navbar-mobile__link')
+const navbarList = document.querySelector('.navbar-mobile__list')
 
 const setActiveLink = (anchor) => {
 	navbarlink.forEach((navItem) => {
-		navItem.classList.toggle('active', anchor === navItem.getAttribute('href'));
-	});
-};
+		navItem.classList.toggle('active', anchor === navItem.getAttribute('href'))
+	})
+}
 
 const navBarClickHandler = (e) => {
-	const link = e.target.closest('a');
+	const link = e.target.closest('a')
 
 	if (!link) {
-		return;
+		return
 	}
 
-	const anchor = link.getAttribute('href');
-	setActiveLink(anchor);
-};
+	const anchor = link.getAttribute('href')
+	setActiveLink(anchor)
+}
 
-navbarList.addEventListener('click', navBarClickHandler);
+navbarList.addEventListener('click', navBarClickHandler)
 
 /* header-popup */
 
 try {
 	popupBtnClose.addEventListener('click', () => {
-		_slideUp(popup);
-	});
+		_slideUp(popup)
+	})
 } catch (error) {
-	console.log(error);
+	console.log(error)
 }
 
 /* viewBtn Catalog page */
@@ -106,53 +106,53 @@ try {
 try {
 	cardCatalog.forEach((card, i) => {
 		card.addEventListener('click', function ({ target }) {
-			const viewBtn = card.querySelector('.card-catalog__view-button');
-			const cardDesc = card.querySelector('.card-catalog__desc');
-			const cardCatalogLabels = card.querySelector('.card-catalog__labels');
+			const viewBtn = card.querySelector('.card-catalog__view-button')
+			const cardDesc = card.querySelector('.card-catalog__desc')
+			const cardCatalogLabels = card.querySelector('.card-catalog__labels')
 
 			if (target.closest('.card-catalog__view-button')) {
-				cardDesc.classList.toggle('active');
-				viewBtn.classList.toggle('active');
-				cardCatalogLabels.classList.toggle('active');
+				cardDesc.classList.toggle('active')
+				viewBtn.classList.toggle('active')
+				cardCatalogLabels.classList.toggle('active')
 			}
-		});
-	});
+		})
+	})
 } catch (error) {
-	console.log(error);
+	console.log(error)
 }
 
 /* copy promocode text */
 
 try {
-	const btnPromocode = document.querySelector('.promocode__button');
-	const promocodeNotion = document.querySelector('.promocode__notion');
+	const btnPromocode = document.querySelector('.promocode__button')
+	const promocodeNotion = document.querySelector('.promocode__notion')
 
 	const copyText = () => {
 		// Select the email link anchor text
-		const promocode = document.querySelector('.promocode__name');
-		const range = document.createRange();
-		range.selectNode(promocode);
-		window.getSelection().addRange(range);
+		const promocode = document.querySelector('.promocode__name')
+		const range = document.createRange()
+		range.selectNode(promocode)
+		window.getSelection().addRange(range)
 
 		try {
 			// Now that we've selected the anchor text, execute the copy command
-			const successful = document.execCommand('copy');
-			const msg = successful ? 'successful' : 'unsuccessful';
-			console.log('Copy email command was ' + msg);
-			promocodeNotion.classList.add('active');
+			const successful = document.execCommand('copy')
+			const msg = successful ? 'successful' : 'unsuccessful'
+			console.log('Copy email command was ' + msg)
+			promocodeNotion.classList.add('active')
 			setTimeout(() => {
-				promocodeNotion.classList.remove('active');
-			}, 1000);
+				promocodeNotion.classList.remove('active')
+			}, 1000)
 		} catch (err) {
-			console.log('Oops, unable to copy');
+			console.log('Oops, unable to copy')
 		}
 
 		// Remove the selections - NOTE: Should use
 		// removeRange(range) when it is supported
-		window.getSelection().removeAllRanges();
-	};
+		window.getSelection().removeAllRanges()
+	}
 
-	btnPromocode.addEventListener('click', copyText);
+	btnPromocode.addEventListener('click', copyText)
 } catch (error) {
-	console.log(error);
+	console.log(error)
 }
